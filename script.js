84178764a22ef06ff25e68b9c3b14b6fd3ff55a4
jeffclaybrook <48254646+jeffclaybrook@users.noreply.github.com>
2021-09-27 22:10:29 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ let menuIcon = document.getElementById("menu-icon");
 menuIcon.addEventListener("click", function() {
    document.querySelector("nav").classList.toggle("expanded");
 })
+
+let navLink = document.querySelectorAll("li");
+for (let i=0; i<navLink.length; i++) {
+   navLink[i].onclick = function() {
+      let j = 0;
+      while(j < navLink.length) {
+         navLink[j++].className = "list";
+      }
+      navLink[i].className = "list active";
+   }
+}
